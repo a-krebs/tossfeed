41e64165bd611a15e9df12fb8e7b2d39bc713b9d
Aaron Krebs <akrebs@ualberta.ca>
2013-03-07 18:44:01 -0700

    Initial feed models.

--- tossfeed/feeds/models.py
+++ tossfeed/feeds/models.py
@@ -1,3 +1,15 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Feed(models.Model):
+    token = models.IntegerField()
+    date_created = models.DateFieldTimeField(auto_now_add=True)
+    date_last_posted = models.DateTimeField(auto_now=True)
+    date_last_viewed = models.DateTimeField(default=datetime.now)
+    views = models.IntegerField(default = 0)
+
+class Item(models.Model):
+    feed = models.ForeignKey(Feed, related_name='items')
+    date_posted = models.DateTimeField(auto_now_add=True)
+    url = models.URLField()
