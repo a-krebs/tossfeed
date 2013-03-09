bdf700795f1122e5e7f77250cfeac3e079399afd
Aaron Krebs <akrebs@ualberta.ca>
2013-03-09 11:13:40 -0700

    Implemented Feed view.

--- tossfeed/feeds/views.py
+++ tossfeed/feeds/views.py
@@ -3,23 +3,22 @@ from django.core.urlresolvers import reverse
 from feeds import Feed as FeedModel
 
 class TossFeed(Feed):
-    title = "Chicagocrime.org site news"
-    link = "/sitenews/"
-    description = "Updates on changes and additions to chicagocrime.org."
     def __init__(self, *args, **kwargs):
         self.feed = get_object_or_404(FeedModel, pk=kwargs.pop('feed_id', -1))
-        self.title = self.feed.id
+        self.title = self.feed.name
+        self.link = reverse('feeds:index', feed_id=self.feed.id)
+        self.description = 'TossFeed'
         super(TossFeed, self).__init__(*args, **kwargs)
 
     def items(self):
-        return NewsItem.objects.order_by('-pub_date')[:5]
+        return self.feed.objects.order_by('-date_posted')
 
     def item_title(self, item):
-        return item.title
+        return item.url
 
     def item_description(self, item):
-        return item.description
+        return item.url
 
     # item_link is only needed if NewsItem has no get_absolute_url method.
     def item_link(self, item):
-        return reverse('news-item', args=[item.pk])
+        return item.url
