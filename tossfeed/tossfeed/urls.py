ecb7eb36ea804981b30378e1fe58adb81360879d
Aaron Krebs <akrebs@ualberta.ca>
2013-03-09 22:02:08 -0700

    Base css file. Set up URL to serve up static files in dev environment, and added base css file. Modified base.html to include title and tagline.

--- tossfeed/tossfeed/urls.py
+++ tossfeed/tossfeed/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, url
 from feeds import views
 
+
 urlpatterns = patterns('',
     url(r'^(?P<feed_id>\d+)/$', views.TossFeed(), name='feed'),
     url(r'^(?P<feed_id>\d+)/detail$', views.FeedDetailView.as_view(), name='detail'),
     url(r'^(?P<feed_id>\d+)/add$', views.AddToFeed.as_view(), name='add'),
     url(r'^$', views.CreateFeed.as_view(), name='index'),
 )
+
+# TODO: remove before deployment
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
