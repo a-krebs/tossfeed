645f928b49056c41bc3cb42c9e44460122a4ca04
Aaron Krebs <akrebs@ualberta.ca>
2013-03-10 01:36:22 -0700

    Added favicon and robots.txt

--- tossfeed/tossfeed/urls.py
+++ tossfeed/tossfeed/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView, RedirectView
 from feeds import views
 
 
 urlpatterns = patterns('',
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/res/favicon.ico')),
     url(r'^(?P<feed_id>\d+)/$', views.TossFeed(), name='feed'),
     url(r'^(?P<feed_id>\d+)/detail$', views.FeedDetailView.as_view(), name='detail'),
     url(r'^(?P<feed_id>\d+)/add$', views.AddToFeed.as_view(), name='add'),
